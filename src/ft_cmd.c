8700d2ff0816e6d797c98cd3c1beba209ae8a3ec
Ismail Ait bella <iait-bel@e2r5p1.1337.ma>
2021-12-02 01:04:04 +0100

    fix: bonus

--- src/ft_cmd.c
+++ src/ft_cmd.c
@@ -39,13 +39,14 @@ char *get_cmd_path(char *cmd, char **env)
 }
 
 
-int _exec(char *cmd, int *fds, int fd_to, char **env)
+int _exec(char *cmd, int *fds, char **env)
 {
 	char **args;
 	char *file;
 	int pid;
 	int state;
-
+	
+	fds = fds + 1;
 	args = ft_split(cmd, ' ');
 	file = get_cmd_path(args[0], env);
 	free(args[0]);
@@ -57,20 +58,46 @@ int _exec(char *cmd, int *fds, int fd_to, char **env)
 	}
 	else if(pid == 0)
 	{
-		dup2(fds[fd_to], fd_to);
-		close(fds[0]);
-		close(fds[1]);
 		state = execve(file, args, env);
 		exit(state != -1);
 	}
 	return pid;
 }
 
+int	*execmap(char **cmds, int fd, char **env)
+{
+	int i;
+	int count;
+	int fildes[2];
+	int *pids;
+	
+	count = 0;
+	while(cmds[count])
+		count++;
+	pids = malloc(sizeof(int *) * count);
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	i = 0;
+	while(cmds[i])
+	{
+		dup2(fildes[1], STDOUT_FILENO);
+		dup2(fildes[0], STDIN_FILENO);
+		close(fildes[0]);
+		close(fildes[1]);
+		if (cmds[i + 1] == 0)
+			dup2(4, STDOUT_FILENO);
+		pids[i] = _exec(cmds[i], fildes, env);
+		i++;
+	}
+	return pids;
+}
+
+
+
 int exec_cmd(char **cmds, int fd, char **env)
 {
 	int fildes[2];
-	int pid1;
-	int pid2;
+	int *pids;
 	int i;
 
 	if(pipe(fildes) == -1)
@@ -78,24 +105,13 @@ int exec_cmd(char **cmds, int fd, char **env)
 		
 		return 0;
 	}
+	pids = execmap(cmds, fd, env);
 
-	dup2(fd, STDIN_FILENO);
-	i = 0;
-	pid1 = _exec(cmds[i], fildes, STDOUT_FILENO, env);
-	while(cmds[i] && cmds[i + 1])
-	{
-		pid2 = _exec(cmds[i + 1], fildes, STDIN_FILENO, env);
-		if (cmds[i + 1] == 0)
-		{
-			dup2(4, STDOUT_FILENO);
-		}
-		i++;
-	}
 	close(fildes[0]);
 	close(fildes[1]);
-
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+	i = 0;
+	while(cmds[i])
+		waitpid(pids[i++], NULL, 0);
 
 	return 0;
 }
